e2df67798ee2719f03abe78f96cef6868535f442
Oliver Renwick <oliver.renwick@gmail.com>
2020-04-03 11:50:01 -0400

    Updates

---
+++ precache-manifest.c1275811cd86eafde940daa31b92b466.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4abb1c7c8e4392894a22c62d313b762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "120b11d674d45b85b03c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "684ef56e7e54af2d74be",
+    "url": "/static/css/main.293701de.chunk.css"
+  },
+  {
+    "revision": "120b11d674d45b85b03c",
+    "url": "/static/js/2.b71b2e37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b71b2e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684ef56e7e54af2d74be",
+    "url": "/static/js/main.b2e64658.chunk.js"
+  },
+  {
+    "revision": "f5569dda282e6c339124",
+    "url": "/static/js/runtime-main.73ff38f6.js"
+  },
+  {
+    "revision": "513f5688f0b7da8245b6425ac4258756",
+    "url": "/static/media/DSC01556.513f5688.JPG"
+  },
+  {
+    "revision": "1afc3927088a84eeac47565298f36189",
+    "url": "/static/media/Openlease.1afc3927.jpg"
+  },
+  {
+    "revision": "d9bace7d90b03c90b374cf626d31b3d9",
+    "url": "/static/media/biod.d9bace7d.jpg"
+  },
+  {
+    "revision": "93b0cd96e98ea70f12aefffab20cab29",
+    "url": "/static/media/boardmembers.93b0cd96.jpg"
+  },
+  {
+    "revision": "dda6ac8e92a5b1f01def07543d02cb94",
+    "url": "/static/media/gbaon.dda6ac8e.png"
+  },
+  {
+    "revision": "f02257ed0cd6c3c92f3b6fc240d9814a",
+    "url": "/static/media/githubprof.f02257ed.jpg"
+  },
+  {
+    "revision": "97c45c7cc154478df61e0c8a8b79eb31",
+    "url": "/static/media/knowyourrights.97c45c7c.jpg"
+  },
+  {
+    "revision": "56887a0562fe42ec6798179ab039ef53",
+    "url": "/static/media/legaltranslator.56887a05.JPG"
+  },
+  {
+    "revision": "5b4f168cab00736e596a2e1573a884d7",
+    "url": "/static/media/linkedin.5b4f168c.jpg"
+  },
+  {
+    "revision": "797edbb1b75fdb3923ac7638df2c905d",
+    "url": "/static/media/nodeserv.797edbb1.jpg"
+  },
+  {
+    "revision": "95ee8e8c9e6c1ad79cd1efc67434fd5d",
+    "url": "/static/media/pokemongym.95ee8e8c.jpg"
+  },
+  {
+    "revision": "420b3c4c5bef3b0a6e38a357da12e834",
+    "url": "/static/media/qpqhomepage.420b3c4c.jpg"
+  },
+  {
+    "revision": "5c4f04a0b97f90398529ff7f0ddf8b86",
+    "url": "/static/media/rps.5c4f04a0.jpg"
+  },
+  {
+    "revision": "187a63042bb9258e8b6020b6da3a735b",
+    "url": "/static/media/sevilladusk.187a6304.jpg"
+  },
+  {
+    "revision": "d6e66d19e0f87e5d38d4aa6e59710f7d",
+    "url": "/static/media/small_headshot.d6e66d19.jpg"
+  },
+  {
+    "revision": "37b3e9da89de2548ed73597b036c0b4f",
+    "url": "/static/media/spires.37b3e9da.jpg"
+  },
+  {
+    "revision": "97fea2754accd946d93e42d8c11e506c",
+    "url": "/static/media/triplethreat.97fea275.jpg"
+  },
+  {
+    "revision": "19efd8bf54784f13418e696a84931d13",
+    "url": "/static/media/twitterpage.19efd8bf.jpg"
+  },
+  {
+    "revision": "8e78c8a90508cc4beab97a15cca55a3c",
+    "url": "/static/media/yasaqi.8e78c8a9.jpg"
+  }
+]);
