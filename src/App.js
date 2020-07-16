3bb050cdbd9f8084d8edccdc94c31e4ee596fa3b
oliver <oliver.renwick@gmail.com>
2020-07-15 23:18:36 -0400

    Re-add hash links! <3 kumavis

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React, {Component } from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
+HashRouter, Route, Link
+
 } from "react-router-dom";
 import Home from "./home";
-import Channels from "./channels";
 import Portfolio from "./portfolio";
+import Channels from "./channels";
 
-export default function App() {
-  return (
-    <Router>
-        <Switch>
-          <Route path="/portfolio">
-            <Portfolio />
-          </Route>
-          <Route path="/channels">
-            <Channels />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return(
+      <HashRouter basename='/'>
+        <Route exact path="/" component={Home} />
+        <Route path="/portfolio" component={Portfolio}/>
+        <Route path="/channels" component={Channels}/>
+      </HashRouter>
+    );
+  }
 }
+
+export default App; 
+
+// export default function App() {
+//   return (
+//     <Router>
+//         <Switch>
+//           <Route path="/portfolio">
+//             <Portfolio />
+//           </Route>
+//           <Route path="/channels">
+//             <Channels />
+//           </Route>
+//           <Route path="/">
+//             <Home />
+//           </Route>
+//         </Switch>
+//     </Router>
+//   );
+// }
 
 // function About() {
 //   return Home;
